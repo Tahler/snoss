8ac3fd595f7d5e09f8ff32fa68dfbe51fe9cc7af
Tyler Berry <tyler@berryac.com>
2017-02-10 12:27:20 -0700

    time_utils.rs more usable

--- src/time_utils.rs
+++ src/time_utils.rs
@@ -1,6 +1,8 @@
+// Re-export time crate
+pub use time::*;
 use time;
 
-fn since(time: &time::Tm) -> time::Duration {
+pub fn since(time: &time::Tm) -> time::Duration {
     let now = time::now().to_timespec();
     let tm_spec = time.to_timespec();
     now - tm_spec
